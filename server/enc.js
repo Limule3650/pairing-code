0e6c60e3140c2b28c6bc8d386edde1e87d013735
Limule3650 <152185817+Limule3650@users.noreply.github.com>
2024-10-13 18:48:24 +0100

    Update enc.js

--- server/enc.js
+++ server/enc.js
@@ -25,7 +25,7 @@ b.map((m)=>{
 }
 
 function decrypt(session){
-    session = session.replace(/inrl~/,'');
+    session = session.replace(/vanitas~/,'');
     let b = session.split("")
     let c = "",l="",d="",t;
     b.map((m)=>{
